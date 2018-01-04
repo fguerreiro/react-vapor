e1d11ca0d10b652e9b2034a26efbbb7de0dfa582
Francisco Guerreiro <fguerreiro@coveo.com>
2018-01-04 12:13:09 -0500

    indentation

--- src/components/navigationLoader/NavigationLoaderExample.tsx
+++ src/components/navigationLoader/NavigationLoaderExample.tsx
@@ -9,27 +9,27 @@ export class NavigationLoaderExample extends React.Component<any, any> {
       <div className='mt2'>
         <label className='form-control-label'>Navigation Loading</label>
         <div className='flex flex-row flex-stretch'>
-        <NavigationContainer className={'full-content-y'}>
-          <NavigationMenuSection>
-            <NavigationMenuSectionItem classes={['mod-width-30']} />
-            <NavigationMenuSectionItem classes={['mod-width-50']} />
-            <NavigationMenuSectionItem classes={['mod-width-40']} />
-            <NavigationMenuSectionItem classes={['mod-width-60']} />
-            <NavigationMenuSectionItem classes={['mod-width-50']} />
-          </NavigationMenuSection>
-          <NavigationMenuSection>
-            <NavigationMenuSectionItem classes={['mod-width-30']} />
-            <NavigationMenuSectionItem classes={['mod-width-50']} />
-            <NavigationMenuSectionItem classes={['mod-width-60']} />
-            <NavigationMenuSectionItem classes={['mod-width-30']} />
-          </NavigationMenuSection>
-          <NavigationMenuSection>
-            <NavigationMenuSectionItem classes={['mod-width-30']} />
-            <NavigationMenuSectionItem classes={['mod-width-40']} />
-            <NavigationMenuSectionItem classes={['mod-width-50']} />
-            <NavigationMenuSectionItem classes={['mod-width-30']} />
-          </NavigationMenuSection>
-        </NavigationContainer>
+          <NavigationContainer className={'full-content-y'}>
+            <NavigationMenuSection>
+              <NavigationMenuSectionItem classes={['mod-width-30']} />
+              <NavigationMenuSectionItem classes={['mod-width-50']} />
+              <NavigationMenuSectionItem classes={['mod-width-40']} />
+              <NavigationMenuSectionItem classes={['mod-width-60']} />
+              <NavigationMenuSectionItem classes={['mod-width-50']} />
+            </NavigationMenuSection>
+            <NavigationMenuSection>
+              <NavigationMenuSectionItem classes={['mod-width-30']} />
+              <NavigationMenuSectionItem classes={['mod-width-50']} />
+              <NavigationMenuSectionItem classes={['mod-width-60']} />
+              <NavigationMenuSectionItem classes={['mod-width-30']} />
+            </NavigationMenuSection>
+            <NavigationMenuSection>
+              <NavigationMenuSectionItem classes={['mod-width-30']} />
+              <NavigationMenuSectionItem classes={['mod-width-40']} />
+              <NavigationMenuSectionItem classes={['mod-width-50']} />
+              <NavigationMenuSectionItem classes={['mod-width-30']} />
+            </NavigationMenuSection>
+          </NavigationContainer>
         </div>
       </div>
     );
